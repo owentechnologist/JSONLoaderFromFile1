05b1a2ffa6be9404f9e5ac755fb23ae0023df25c
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2023-01-03 10:32:06 -0600

    Updated ConnectionHelper

--- src/main/java/com/redislabs/sa/ot/jlff/Main.java
+++ src/main/java/com/redislabs/sa/ot/jlff/Main.java
@@ -70,26 +70,11 @@ public class Main {
             }
         }
         try{
-            connectionHelper = new ConnectionHelper(buildURI(host,port,username,password));
+            connectionHelper = new ConnectionHelper(ConnectionHelper.buildURI(host,port,username,password));
             loadJSONDataFromFile(filepath,connectionHelper);
             manipulateJsonData(connectionHelper);
         }catch(Throwable t){t.printStackTrace();}
 
-    }
-
-    static URI buildURI(String host,int port,String username,String password){
-        URI uri = null;
-        try {
-            if (!("".equalsIgnoreCase(password))) {
-                uri = new URI("redis://" + username + ":" + password + "@" + host + ":" + port);
-            } else {
-                uri = new URI("redis://" + host + ":" + port);
-            }
-        } catch (URISyntaxException use) {
-            use.printStackTrace();
-            System.exit(1);
-        }
-        return uri;
     }
 
     static int loadJSONDataFromFile(String path,ConnectionHelper connectionHelper) throws Throwable{
@@ -149,17 +134,54 @@ class ConnectionHelper{
     final PooledConnectionProvider connectionProvider;
     final JedisPooled jedisPooled;
 
+    /**
+     * Used when you want to send a batch of commands to the Redis Server
+     * @return Pipeline
+     */
     public Pipeline getPipeline(){
-        return new Pipeline(connectionProvider.getConnection());
+        return  new Pipeline(jedisPooled.getPool().getResource());
     }
 
+    /**
+     * Assuming use of Jedis 4.3.1:
+     * https://github.com/redis/jedis/blob/82f286b4d1441cf15e32cc629c66b5c9caa0f286/src/main/java/redis/clients/jedis/Transaction.java#L22-L23
+     * @return Transaction
+     */
     public Transaction getTransaction(){
-        return new Transaction(connectionProvider.getConnection());
+        return new Transaction(jedisPooled.getPool().getResource());
     }
 
+    /**
+     * Obtain the default object used to perform Redis commands
+     * @return JedisPooled
+     */
     public JedisPooled getPooledJedis(){
         return jedisPooled;
     }
+
+    /**
+     * Use this to build the URI expected in this classes' Constructor
+     * @param host
+     * @param port
+     * @param username
+     * @param password
+     * @return
+     */
+    public static URI buildURI(String host,int port,String username,String password){
+        URI uri = null;
+        try {
+            if (!("".equalsIgnoreCase(password))) {
+                uri = new URI("redis://" + username + ":" + password + "@" + host + ":" + port);
+            } else {
+                uri = new URI("redis://" + host + ":" + port);
+            }
+        } catch (URISyntaxException use) {
+            use.printStackTrace();
+            System.exit(1);
+        }
+        return uri;
+    }
+
 
     public ConnectionHelper(URI uri){
         HostAndPort address = new HostAndPort(uri.getHost(), uri.getPort());
@@ -179,7 +201,7 @@ class ConnectionHelper{
         }
         GenericObjectPoolConfig<Connection> poolConfig = new ConnectionPoolConfig();
         poolConfig.setMaxIdle(10);
-        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxTotal(1000);
         poolConfig.setMinIdle(1);
         poolConfig.setMaxWait(Duration.ofMinutes(1));
         poolConfig.setTestOnCreate(true);
